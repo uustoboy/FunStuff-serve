5e1b74c4304cb390d468320a62c1529f26e4e9ae
uustoboy <tianqi_email@163.com>
2022-06-15 14:14:26 +0800

    add

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,11 @@
 const { exec,escape } = require('../db/mysql')
+const {genPassword} = require('../utils/cryp')
 const login = (username,password)=>{
     username = escape(username);
+    // 生成加密码
+    password = genPassword(password)
     password = escape(password);
+
     const sql = `select username,password from users where username=${username} and password=${password}`
     return exec(sql).then(rows=>{
         return rows[0] || {}
